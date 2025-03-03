8d33d0869857a3c290a802c9b289b6f033b63239
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2025-03-03 22:14:39 +0530

    d3

--- server/config/db.js
+++ server/config/db.js
@@ -5,10 +5,7 @@ dotenv.config();
 
 const db = async()=>{
     try{
-      await mongoose.connect(process.env.MONGO_URL,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+      await mongoose.connect(process.env.MONGO_URL);
       console.log("MongoDB connected successfully!");
     }
     catch(error){
