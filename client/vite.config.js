fd860da5203e4a1621776c8cdfb62f26b630c57c
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2025-03-03 22:08:01 +0530

    backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080", // Backend URL
+        target: "https://ticketapp-jz7r.onrender.com", // Backend URL
         // target: process.env.VITE_API_BASE_URL, // Use environment variable
         changeOrigin: true,
       },
